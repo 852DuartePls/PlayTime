1f2ab5fc605828587051f0aed199d405944471e1
852DuartePls <duartebenitez_001@hotmail.com>
2024-10-27 15:03:21 -0300

    Implemented new method to retrieve a List of all players inside the data file

--- src/main/java/me/f64/playtime/utils/DataStorage.java
+++ src/main/java/me/f64/playtime/utils/DataStorage.java
@@ -5,6 +5,8 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class DataStorage {
@@ -37,7 +39,7 @@ public class DataStorage {
         return DriverManager.getConnection(databaseUrl);
     }
 
-    public void savePlayerData(Player player, int playTime, int joins, int session) {
+    public void savePlayerData(@NotNull Player player, int playTime, int joins, int session) {
         String query = "INSERT OR REPLACE INTO players (uuid, playerName, time, joins, session) VALUES (?, ?, ?, ?, ?)";
         try (Connection connection = connect();
              PreparedStatement statement = connection.prepareStatement(query)) {
@@ -53,7 +55,7 @@ public class DataStorage {
         }
     }
 
-    public PlayerData loadPlayerData(UUID uuid) {
+    public PlayerData loadPlayerData(@NotNull UUID uuid) {
         String query = "SELECT * FROM players WHERE uuid = ?";
         try (Connection connection = connect();
              PreparedStatement statement = connection.prepareStatement(query)) {
@@ -73,6 +75,28 @@ public class DataStorage {
             plugin.getLogger().warning("Could not load player data: " + e);
         }
         return null;
+    }
+
+    public List<PlayerData> loadAllPlayerData() {
+        List<PlayerData> players = new ArrayList<>();
+        String query = "SELECT * FROM players";
+        try (Connection connection = connect();
+             PreparedStatement statement = connection.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                UUID uuid = UUID.fromString(resultSet.getString("uuid"));
+                String playerName = resultSet.getString("playerName");
+                int playTime = resultSet.getInt("time");
+                int joins = resultSet.getInt("joins");
+                int session = resultSet.getInt("session");
+
+                players.add(new PlayerData(uuid, playerName, playTime, joins, session));
+            }
+        } catch (SQLException e) {
+            plugin.getLogger().warning("Could not load all player data: " + e);
+        }
+        return players;
     }
 
     public record PlayerData(UUID uuid, String playerName, int playTime, int joins, int session) {
